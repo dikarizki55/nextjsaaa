ad6f4c6c73667800093d940cfeabfc1d1a61981b
dikarizki55 <dikaesmidorikawa55@gmail.com>
2023-09-09 12:31:12 +0700

    ganti navbar

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='w-100 bg-red-500 text-white text-center text-6xl p-5'>Navbar</div>
+    <div>
+      <div className='w-100 bg-red-500 text-white text-center text-6xl p-5'>Navbar</div>
+      <p>misal nih aku tambahin aku rubah sekarang nih kok harus refresh dulu padahal kalo di komputer gk perlu pake refresh refresh segala sih <br />
+      gapapalah yang penting jalan</p>
+    </div>
+    
   )
 }
