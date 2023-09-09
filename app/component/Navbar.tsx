581e54927b498b927f3349ece9c876dd277e8b23
dikarizki55 <dikaesmidorikawa55@gmail.com>
2023-09-09 13:56:40 +0700

    ganti navbar

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <div>
       <div className='w-100 bg-red-500 text-white text-center text-6xl p-5'>Navbar</div>
-      <p>misal nih aku tambahin aku rubah sekarang nih kok harus refresh dulu padahal kalo di komputer gk perlu pake refresh refresh segala sih <br />
-      gapapalah yang penting jalan</p>
+      <p className='mb-5 mt-5'>misal nih aku tambahin aku rubah</p>
+      <Link href="/" className="bg-blue-500 p-3 hover:bg-gray-500 transition-all duration-1000 hover:transition-all hover:duration-1000 rounded-full text-white uppercase">Home</Link>
+      <Link href="/default" className="bg-blue-500 p-3 hover:bg-gray-500 transition-all duration-1000 hover:transition-all hover:duration-1000 rounded-full text-white uppercase">linkaja</Link>
+
     </div>
     
   )
