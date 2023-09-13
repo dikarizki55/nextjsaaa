b43907e78e3305ebe7701edaccd887c27d97be85
Dika Rizki <dikaesmidorikawa55@gmail.com>
2023-09-13 23:38:37 +0700

    sequential fetch data

--- app/cari/[slug]/page.tsx
+++ app/cari/[slug]/page.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 async function getData(param:string) {
   const res = await fetch(`https://api.github.com/users/${param}`)
   return res.json()
+}
+async function getDataSearch(param:string) {
+  const res = await fetch(`https://api.github.com/search/users?q=${param}`)
+  await new Promise(r=>setTimeout(r, 3000))
+  return res.json()
+}
+
+async function DataSearch({query}:any){
+  const data = await getDataSearch(query)
+  return(
+    <div>
+      <p>Detail Search : {query}</p>
+      <div>
+        {JSON.stringify(data)}
+      </div>
+    </div>
+  )
 }
 
 export default async function DetailCari({params}:{params:{slug:string}}) {
@@ -13,6 +30,12 @@ export default async function DetailCari({params}:{params:{slug:string}}) {
       <div>
         {JSON.stringify(data)}
       </div>
+      <hr></hr>
+      <hr></hr>
+      <hr></hr>
+      <Suspense fallback={<div>Loading brooo.... SABAR ANJINKKK!!!!</div>}>
+        <DataSearch query={params.slug} />
+      </Suspense>
     </div>
   )
 }
